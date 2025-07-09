82bd4ed6b5be8750f0e379f36783de7d5892eca2
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-09 19:56:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to Sniply, Only the posts worth your time")
+print("Please choose an option:")
+print("1- Start")
+print("2- View connected Social Media")
+print("3- Add new account")
+choice = input("Enter your choice: ")
+
+if choice == '1':
+    
