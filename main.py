28c3a0f59fd54b9dbf2de7d798962af09b7a79b6
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-14 02:35:26 +0300

    made all three options working.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+
+from add_account import add_account
+from start import start
+from view_accounts import viewAccounts
+
+
 print("Welcome to Sniply, Only the posts worth your time")
 print("Please choose an option:")
 print("1- Start")
@@ -6,4 +13,11 @@ print("3- Add new account")
 choice = input("Enter your choice: ")
 
 if choice == '1':
-    
+    start()
+elif choice == '2':
+    viewAccounts()
+elif choice == '3':
+    add_account()
+else:
+    print("Bye, see you soon!")
+    sys.exit()
