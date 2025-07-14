0dcf745e76d6207ad4008e2fa5417eded509b6b7
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-15 01:01:27 +0300

    update main to add the new manage account part

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import sys
+from colorama import Fore
 
 from add_account import add_account
 from start import start
 from view_accounts import viewAccounts
+from manage_accounts import manage_accounts
 
+print(Fore.GREEN + """
+ ░▒▓███████▓▒░▒▓███████▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ 
+ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░    ░▒▓██████▓▒░  
+       ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░     
+       ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░     
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓████████▓▒░▒▓█▓▒░     
+                                                                      
+""" + Fore.RESET)
 
 print("Welcome to Sniply, Only the posts worth your time")
 print("Please choose an option:")
-print("1- Start")
-print("2- View connected Social Media")
-print("3- Add new account")
+print("1- Start Scraping")
+print("2- View Connected Accounts")
+print("3- Manage Accounts (Settings, Delete, Re-auth)")
+print("4- Add New Account")
+print("5- Exit")
 choice = input("Enter your choice: ")
 
 if choice == '1':
@@ -17,7 +31,12 @@ if choice == '1':
 elif choice == '2':
     viewAccounts()
 elif choice == '3':
+    manage_accounts()
+elif choice == '4':
     add_account()
-else:
+elif choice == '5':
     print("Bye, see you soon!")
+    sys.exit()
+else:
+    print("Invalid choice. Please run the program again.")
     sys.exit()
